7fcd0f175940d1ccf18656e2dce07a9140ac3900
Youngsoo, Na <69225568+newpullin@users.noreply.github.com>
2020-11-27 08:43:30 +0900

    for 문을 바꿔서 성공

--- programmers/p18_findingPrimeNumber.py
+++ programmers/p18_findingPrimeNumber.py
@@ -1,20 +1,18 @@
 from itertools import permutations
-
+import math
 
 def solution(number: str) -> int:
     n = len(number)
     possible = set(map(''.join, permutations(number)))
-    max_value = int(max(possible))
+
     for i in range(1, n):
         possible.update(set(map(''.join, permutations(number, i))))
     #set 숫자로 변환
     possible = set(map(int, possible))
-    eratos = [x for x in range(3, max_value+1, 2)]
-    primes = []
-    while len(eratos) > 0:
-        v = eratos.pop(0)
-        primes.append(v)
-        eratos = [x for x in eratos if x % v != 0]
+
+    # 1제거
+    if 1 in possible:
+        possible.remove(1)
 
     count = 0
     for num in possible:
@@ -25,10 +23,15 @@ def solution(number: str) -> int:
         if num % 2 == 0:
             continue
 
-        if num in primes:
-            count += 1
+        prime = True
+        for i in range(3, int(math.sqrt(num)+1), 2):
+            if num % i == 0:
+                prime = False
+                break
 
+        if prime:
+            count += 1
     return count
 
 
-print(solution("7843"))
+print(solution("011"))
