e539d93771d6f14777fcc3b1bc00105c1e441f29
Youngsoo, Na <newpullin@naver.com>
2020-12-07 01:35:34 +0900

    집단지능을 이용한 최적화..

--- programmers/p29_SpeedCamera_l3.py
+++ programmers/p29_SpeedCamera_l3.py
@@ -1,19 +1,14 @@
-from collections import deque
-
-
 def solution(routes):
-    sorted_routes = deque(sorted(routes, key=lambda x: x[1]))
+    sorted_routes = sorted(routes, key=lambda x: x[1])
 
-    camera_pos = sorted_routes.popleft()[1]
-    answer = 1
-    while sorted_routes:
-        while sorted_routes and sorted_routes[0][0] <= camera_pos:
-            sorted_routes.popleft()
-        if sorted_routes:
-            camera_pos = sorted_routes.popleft()[1]
+    camera_pos = -30000
+    answer = 0
+    for route in sorted_routes:
+        # 설치된 카메라 위치보다 왼쪽에 있으면 패스
+        # 입구가 설치된 카메라 위치보다 오른쪽에 있으면 카메라 위치는 출구
+        if route[0] > camera_pos:
+            camera_pos = route[1]
             answer += 1
-        else:
-            break
 
     return answer
 
